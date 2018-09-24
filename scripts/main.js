41d1598940ec53a58a8a5c4fcc1443304c52447c
turtlesquared <turtlesbills@icloud.com>
2018-09-24 19:48:39 -0400

    fixed the null problem. Missing ending quotes.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,3 @@
-var DETAIL_IMAGE_SELECTOR = '[data-image-role="target]';
-var DETAIL_TITLE_SELECTOR = '[data-image-role="title]';
-var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger]';
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
