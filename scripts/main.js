b3c6fcf0f12e98071a1c43a7284c53e699d7b2c1
turtlesquared <turtlesbills@icloud.com>
2018-09-25 18:45:57 -0400

    Starting to learn some functions.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+
+function setDetails() {
+	'use strict';
+	// Code will go here
+}
